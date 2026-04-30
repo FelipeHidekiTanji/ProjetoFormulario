275c835ab1dfa0334683bda7eee5b1e5d294fd13
FelipeHidekiTanji <felipe.tanji@escola.pr.gov.br>
2026-04-30 11:15:00 -0300

    hideki

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function salvarCarro(event) {
     let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
     let carro = {
+        id: Date.now(),
         titulo,
         preco,
         marca,
@@ -28,23 +29,26 @@ function salvarCarro(event) {
 }
 
 function adicionarNaTela(carro) {
-    let lista = document.getElementById('listarCarros');
+    let lista = document.getElementById('listaCarros');
     let card = document.createElement('div');
     card.classList.add('card');
+
     card.innerHTML = `
+    <img src = "https://picsum.photos/250/150?random = ${Math.random()}">
     <h3>${carro.titulo}</h3>
     <p><strong>Preço:</strong> R$ ${carro.preco}</p>
     <p><strong>Marca:</strong> ${carro.marca}</p>
     <p><strong>Modelo:</strong> ${carro.modelo}</p>
     <p><strong>Câmbio:</strong> ${carro.cambio}</p>
+
+    <button onclick = "excluirCarro(${carro.id}">Excluir</button>
     `
 
     lista.appendChild(card);
-
 }
 
 window.onload = function () {
-    let carros = JSON.parse(localStorage.getItem ('carros')) || [];
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
 
     carros.forEach(carro => {
         adicionarNaTela(carro);
